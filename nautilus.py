e19d1c521110cddede17a04cbb2f2b2f30ec8b61
Seayok <110327347+Seayok@users.noreply.github.com>
2022-09-05 16:39:12 +1000

    v2

--- nautilus.py
+++ nautilus.py
@@ -1,25 +1,28 @@
-def ls(current, cmd, data):
+def ls(current, cmd):
     res = 'Error'
 
     if len(cmd) == 1 or cmd[-1][0] == '-':
-        path = current.path
+        destination = current
     else:
         path = cmd[-1]
-        if path not in data:
-            path = current.path + '/' + cmd[-1]
-            if path not in data:
-                return res
-
+        path = path.split('/')
+        destination = get_path(current, path)
     res = ''
-    tmp = sorted(data[path].children.keys())
+    tmp = sorted(destination.children.keys())
     if "-a" not in cmd and len(tmp) != 1:
-        for key in tmp:
+        index = 0
+        bound = len(tmp)
+        while index < bound:
+            key = tmp[index]
             if key[0] == '.':
-                tmp.pop(tmp.index(key))
+                tmp.pop(index)
+                bound -= 1
+            else:
+                index += 1
 
     if "-l" in cmd:
         for item in tmp:
-            child = data[path].children[item]
+            child = destination.children[item]
             res += child.permission + ' ' + child.owner + ' ' + item + '\n'
     else:
         for item in tmp:
@@ -27,24 +30,23 @@ def ls(current, cmd, data):
 
     return res
 
-def cd(current, path, data):
-    if path in current.children:
-        if current.children[path].permission[0] == 'd':
-            return current.children[path]
-        else:
-            print('cd: Destination is a file')
-            return 'Error'
-    elif path in data:
-        if data[path].permission[0] == 'd':
-            return data[path]
-        else:
-            print('cd: Destination is a file')
-            return 'Error'
-    else:
-        print("cd: No such file or directory")
-        return 'Error'
 
+def ancestor(current):
+    while current.path != '':
+        current = goto(current,['..'])
+    return current
+
+
+def get_path(current, path):
+    if len(path) > 0 and path[0] == '':
+        current = ancestor(current)
+    return goto(current, path)
+
+
+    
 def goto(current, path):
+    if current.permission[3] != 'x':
+        return 'Permission denied'
     if len(path) == 0:
         return current
     name = path[0]
@@ -55,29 +57,65 @@ def goto(current, path):
         return goto(current.parent, path)
     if name in current.children:
         child = current.children[name]
-        if child.permission[0] == 'd' and child.permission[3] == 'x':
+        if child.permission[0] == 'd':
             current = current.children[name]
             return goto(current, path)
-        elif child.permission[0] == '-':
-            return 'Destination was a file'
         else:
-            return 'Permission denied'
+            return [current, name, 'Destination was a file']
     else:
-        return  "No such file or directory"
+        return  [current, name, "No such file or directory"]
 
 
-def mkdir(current, owner, cmd, data):
-    name = cmd[-1].split("/")[-1]
-    if current.path == '/':
-        path = current.path + name
+def mkdir(current, owner, cmd):
+    res = make(current, owner, cmd)
+    if type(res) == str:
+        print(f'{cmd[0]}: {res}')
     else:
-        path = current.path + '/' + name
-    new_folder = Node(current, {}, 'drwxr-x', owner, path)
+        create(res[0], res[1], res[2])
+
+def create(current, owner, name):
+    path = current.path + '/' + name
+    new_folder = Node (current, {}, 'drwxr-x', owner, path)
     current.children[name] = new_folder
-    return new_folder
 
-def touch(current, owner, cmd, data):
-    pass
+
+def make(current, owner, cmd):
+    path = cmd[-1].split('/')
+    name = path[-1]
+    path.pop(-1)
+    cmd[-1] = '/'.join(path)
+    destination = get_path(current, path) 
+    #doi ten lai
+    daddy = ancestor(current) 
+    #rut gon cho nay
+    if type(destination) == list:        
+        if cmd[1] == '-p' and destination[-1] == 'No such file or directory':
+            subname = destination[1]
+            destination = destination[0]
+            create(destination, owner, subname)
+            return make(destination, owner, cmd)
+        else:
+            return"Ancestor directory does not exist"
+    elif destination == 'Permission denied' or daddy == 'Permission denied' or destination.permission[2] == '-':
+        return "Permssion denied"
+    elif name in destination.children:
+        return "File exists"
+    else:
+        return [destination, owner, name]
+
+
+def touch(current, owner, cmd):
+    res = make(current, owner, cmd)
+    if type(res) == str:
+        if res == 'File exists':
+            pass
+        else:
+            print(f'{cmd[0]}: {res}')
+    else:
+        name = cmd[-1].split('/')[-1]
+        path = current.path + '/' + name 
+        new_file = Node(current, {}, '-rw-r--', owner, path)
+        current.children[name] = new_file
 
 def pwd(current):
     return current.path
@@ -93,42 +131,35 @@ class Node:
         
 
 def main():
-    all_files = {}
-
     user = 'root'
     root = Node( None, {}, 'drwxr-x', 'root', '') 
     root.parent = root
-    all_files[""] = root
-
     current = root
 
     while True:
-        cmd = input(f"{user}:/{current.path}$ ")
+        if current == root:
+            cmd = input(f"{user}:/{current.path}$ ")
+        else:
+            cmd = input(f"{user}:{current.path}$ ")
         cmd = cmd.split(" ")
 
         if cmd[0] == "ls":
-            print(ls(current, cmd, all_files), end='')
+            print(ls(current, cmd), end='')
 
         elif cmd[0] == "exit":
             exit(0)
 
         elif cmd[0] == "cd":
             path = cmd[1].split('/')
-            if path[0] == '':
-                result = goto(root, path)
-            else:
-                result = goto(current, path)
-            
+            result = get_path(current, path)
             if type(result) != str:
                 current = result
             else:
                 print(f'cd: {result}')
-
-
+        elif cmd[0] == "touch":
+            touch(current, user, cmd)
         elif cmd[0] == "mkdir":
-            newdir = mkdir(current, user, cmd, all_files)
-            all_files[newdir.path] = newdir
-            
+            mkdir(current, user, cmd)            
 
 if __name__ == '__main__':
     main()
